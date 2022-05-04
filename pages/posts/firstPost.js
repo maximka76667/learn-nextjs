3556d5ccbde79b97f584d9d24bbbc6cec7f51fb9
Maxim <74974283+maximka76667@users.noreply.github.com>
2022-05-04 21:51:47 +0200

    assets-metadata-css lesson

--- pages/posts/firstPost.js
+++ pages/posts/firstPost.js
@@ -1,12 +1,27 @@
 import Link from "next/link";
+import Image from "next/image";
+import Head from "next/head";
+import Script from "next/script";
+import Layout from "../../components/layout";
+
+function ProfileImage() {
+  return (
+    <Image src="/images/profile.jpg" width={144} height={144} alt="Maxim" />
+  )
+}
 
 export default function firstPost() {
   return (
-    <>
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <Script src="https://connect.facebook.net/en_US/sdk.js" strategy="lazyOnload" onLoad={() => console.log("script is loaded")} />
       <h1>first-post</h1>
       <Link href="/">
         <a>Go back</a>
       </Link>
-    </>
+      <ProfileImage />
+    </Layout>
   )
 }
